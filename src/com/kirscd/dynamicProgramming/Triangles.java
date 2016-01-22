a7d8f55e81cb73f9255d848320d846d1c2cd3052
Daniel Kirschner <dpkirschner@gmail.com>
2016-01-21 17:44:29 -0800

    adding dynamic programming version of triangles method

--- src/com/kirscd/dynamicProgramming/Triangles.java
+++ src/com/kirscd/dynamicProgramming/Triangles.java
@@ -21,8 +21,6 @@ import java.util.stream.Stream;
  * 1) All values are positive and single digit
  * 2) Levels do not have to be increasing in length, but they will be filled
  * in from right to left.
- * 3) The depth of the tree must be short enough to avoid a stack overflow.
- * 4) the runtime is garbage because we will be constantly reworking past solutions.
  *
  */
 public class Triangles {
@@ -34,6 +32,18 @@ public class Triangles {
 		return values;
 	}
 	
+	/**
+	 * The runtime for this method will be garbage because we are constantly recalculating 
+	 * values we have already seen. For example, choosing between 5-9 and 5-6 requires us to
+	 * calculate the path 5-9-6-7 as well as 5-6-6-7. The suffix of this path, 6-7, has already been seen.
+	 * If this tree has a depth of 100 for example, we are duplicating a path of length 98, then a path of 97, etc.
+	 * 
+	 * This also has the disadvantage of having to load the entire file in memory at once.
+	 * We can easily stack overflow on even smaller sized trees depending on the amount of RAM etc.
+	 * 
+	 * @param values
+	 * @return
+	 */
 	public static int recursiveHighestTotal(String[] values) {
 		if(values == null || values.length == 0) {
 			return 0;
@@ -60,6 +70,52 @@ public class Triangles {
 		return Math.max(left, right);
 	}
 	
+	/**
+	 * This is a much better solution than the recursive one because we are systematically building
+	 * the correct solution one level at a time. This means two things:
+	 * 1) We never recalculate values we have already done
+	 * 2) We never need to read the whole file into memory (although I have done here to make it easier).
+	 * 
+	 * The runtime here is number of levels*charactersInEachLevel + charactersInLastLevel.
+	 * @param values
+	 * @return
+	 */
+	public static int dynamicProgrammingHighestTotal(String[] values) {
+		if(values == null || values.length == 0) {
+			return 0;
+		}
+		
+		int[][] totals = new int[values.length][values.length];
+		totals[0][0] = charToInt(values[0].charAt(0));
+		
+		int depth = 1;
+		while(depth < values.length) {
+			String level = values[depth];
+			for(int i = 0; i < level.length(); i++) {
+				int thisValue = charToInt(level.charAt(i));
+				
+				int left = 0;
+				if(i > 0) {
+					left = thisValue + totals[depth-1][i-1];
+				}
+				
+				int right = 0;
+				if(i < totals[depth-1].length - 1) {
+					right = thisValue + totals[depth-1][i];
+				}
+				
+				totals[depth][i] = Math.max(left,  right); 
+			}
+			depth++;
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for(int total : totals[values.length-1]) {
+			max = total > max ? total : max;
+		}
+		return max;
+	}
+	
 	private static int charToInt(char c) {
 		return Integer.valueOf(Character.toString(c));
 	}
@@ -73,7 +129,8 @@ public class Triangles {
 			e.printStackTrace();
 		}
 		
-		//String[] values = {"5", "96", "468", "0715"};  
+		//String[] values = {"5", "96", "468", "0715"};
 		System.out.println(recursiveHighestTotal(values));
+		System.out.println(dynamicProgrammingHighestTotal(values));
 	}
 }
